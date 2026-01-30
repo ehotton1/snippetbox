827b360ba4d3df12ff0630cec0229299e8e193b8
Eric Hotton <ehotton1@gmail.com>
2026-01-30 18:05:51 +0000

    serve static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", neuter(fileServer)))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
@@ -16,4 +21,15 @@ func main() {
 
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
+}
+
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
